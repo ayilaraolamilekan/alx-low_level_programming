9bf4edd0eb3212ca0df7bf6053cd71d040974a2b
Sodiq Ayilara <olamilekan.adekunle@gmail.com>
2022-09-08 16:58:34 -0700

    resolve

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+* main - Entry point
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	int s = strlen("and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" -Dora korpa, 2015-10-19\n", s);
 	return (1);
 }
