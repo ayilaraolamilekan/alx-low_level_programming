8c69e0f0983eb491ce667ff7cf9a85e786ffe481
Sodiq Ayilara <olamilekan.adekunle@gmail.com>
2022-09-20 10:45:59 -0700

    resolved

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 		int i = 0;
 			while (s[i] != 0)
-					{
-								i++;
-									}
+			{
+				i++;
+			}
 				while (i > 0)
-						{
-									i--;
-											_putchar(s[i]);
-												}
+				{
+					i--;
+					_putchar(s[i]);
+				}
 					_putchar('\n');
 }
