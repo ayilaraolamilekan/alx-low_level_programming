a524b3260cd37c41c9149ecaf86abc5d2fa5b633
Sodiq Ayilara <olamilekan.adekunle@gmail.com>
2022-09-30 11:04:58 -0700

    i am tired

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,9 +3,13 @@
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
+	int i;
+		for (i = 0; *s != '\0'; i++)
+		{
+			_putchar(*s);
+			s++;
+		}
+		_putchar('\n');	
 }
