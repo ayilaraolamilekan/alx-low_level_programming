8bb98a90001e7f30c48462b10a2f9a9c8f0a1c6c
Sodiq Ayilara <olamilekan.adekunle@gmail.com>
2022-09-30 14:21:11 -0700

    a

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,16 @@
 #include "main.h"
 int _atoi(char *s){
-	return (0);
+	int sign_cache = 1;
+	int result = 0;
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign_cache *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0') * sign_cache;
+		else if (result)
+			break;
+		s++;
+	}
+	return (result);
 }
