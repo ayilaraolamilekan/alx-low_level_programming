2fe1f0c4a963874e119f44079c69479572634a49
Sodiq Ayilara <olamilekan.adekunle@gmail.com>
2022-10-12 05:08:33 -0700

    rem

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - Print a name from a function pointer
  * @name: char string
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f) /* if any is NULL */
+		return;
 	f(name);
 }
