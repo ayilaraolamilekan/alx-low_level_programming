33a65cf67df88d2fe4ed45b9864ca01e16c5321c
Ayilara Sodiq Olamilekan <42064554+ayilaraolamilekan@users.noreply.github.com>
2022-12-12 12:37:48 -0600

    made changes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef _CODING_H_
-#define _CODING_H_
-#include <stdlib.h>
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -16,7 +15,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
